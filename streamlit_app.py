d4effd6afe8e9191c31b832259018952b94253f4
ady400 <raehan223@sma.belajar.id>
2025-04-30 07:13:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
-import streamlit as st
+import random
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+N = 1000  # ukuran lot
+n = 50    # ukuran sampel
+c = 2     # acceptance number
+
+# Simulasi ambil 50 sampel acak dari 1000
+sampel = random.sample(range(1, N + 1), n)
+
+# Misal kita punya data cacat secara acak
+produk_cacat = random.sample(sampel, 3)  # misalnya 3 dari 50 cacat
+
+print("Jumlah cacat:", len(produk_cacat))
+if len(produk_cacat) <= c:
+    print("Lot DITERIMA")
+else:
+    print("Lot DITOLAK")
