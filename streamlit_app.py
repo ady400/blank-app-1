e46504ec7a71635f962ed5d74d18f2b55f73ab61
ady400 <raehan223@sma.belajar.id>
2026-06-05 13:57:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime, date
 import requests
-import os  
+import os
 from streamlit_lottie import st_lottie
 
 # 1. PENGATURAN HALAMAN
